dfd6ab8ba52a382cd2aaf005153da1952ba396dc
FranciscoJSB12 <javif8@gmail.com>
2024-05-08 16:41:45 -0400

    feat: Implement custom TextInput

--- src/presentation/components/ui/CustomView.tsx
+++ src/presentation/components/ui/CustomView.tsx
@@ -1,12 +1,23 @@
 import { ReactNode } from 'react';
-import { StyleProp, Text, View, ViewStyle } from 'react-native';
+import { StyleProp, View, ViewStyle } from 'react-native';
 import { globalStyles } from '../../../config/theme/theme';
 
 interface Props {
   style?: StyleProp<ViewStyle>;
   children: ReactNode;
+  margin?: boolean;
 }
 
-export const CustomView = ({ style, children }: Props) => {
-  return <View style={[globalStyles.mainContainer, style]}>{children}</View>;
+export const CustomView = ({ style, children, margin = false }: Props) => {
+  return (
+    <View
+      style={[
+        globalStyles.mainContainer,
+        style,
+        margin ? globalStyles.globalMargin : null,
+      ]}
+    >
+      {children}
+    </View>
+  );
 };
